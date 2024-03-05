19d5f9bf7150d248b0f1260444363efbb66a9329
Suman Subash <subsum@amazon.com>
2024-03-05 00:18:35 -0800

    generateBid signals

---
+++ dsp1.js
@@ -0,0 +1,23 @@
+function generateBid(
+  interestGroup,
+  auctionSignals,
+  perBuyerSignals,
+  trustedBiddingSignals,
+  browserSignals
+) {
+  var bidCPM = Math.floor(Math.random() * 100, 10);
+  var ad =
+    interestGroup.ads[Math.floor(Math.random() * interestGroup.ads.length)];
+
+  console.log(auctionSignals);
+  console.log(perBuyerSignals);
+  console.log(trustedBiddingSignals);
+  console.log(browserSignals);
+
+  return {
+    ad: ad.metadata,
+    bid: bidCPM,
+    render: ad.renderUrl,
+    allowComponentAuction: true,
+  };
+}
